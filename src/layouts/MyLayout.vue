131b20b677d2b935f27e6ebbe262d0e993cbb48f
Raquel Vuicik <raquel.vuicik@artemidas.net>
2024-11-27 11:20:24 -0300

    atualização de versão quasar

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,23 +1,96 @@
 <template>
-  <q-layout view="hhh lpR fff">
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
 
-    <q-header>
+    <!-- (Optional) The Header -->
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          @click="leftDrawer = !leftDrawer"
+        />
+        <q-toolbar-title>
+          Header
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs>
+        <q-route-tab
+          icon="map"
+          to="/your/route"
+          replace
+          label="One Tab"
+        />
+        <q-route-tab
+          icon="assignment"
+          to="/some/other/route"
+          replace
+          label="Other Tab"
+        />
+      </q-tabs>
     </q-header>
 
+    <!-- (Optional) The Footer -->
+    <q-footer class="bg-grey-8 text-white">
+      <q-tabs switch-indicator>
+        <q-route-tab
+          icon="map"
+          to="/your/route"
+          replace
+          label="One Tab"
+        />
+        <q-route-tab
+          icon="assignment"
+          to="/some/other/route"
+          replace
+          label="Other Tab"
+        />
+      </q-tabs>
+
+      <q-toolbar>
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          @click="leftDrawer = !leftDrawer"
+        />
+        <q-toolbar-title>
+          Footer
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+    <!-- (Optional) A Drawer; you can add one more with side="right" or change this one's side -->
+    <q-drawer
+      v-model="leftDrawer"
+      side="left"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <!-- QScrollArea is optional -->
+      <q-scroll-area class="fit q-pa-sm">
+        <!-- Content here -->
+      </q-scroll-area>
+    </q-drawer>
+
     <q-page-container>
+      <!-- This is where pages get injected -->
       <router-view />
     </q-page-container>
-
-    <q-footer class="bg-grey text-white">
-    </q-footer>
 
   </q-layout>
 </template>
 
 <script>
 export default {
+  name: 'Pagina Inicial',
+
   data () {
     return {
+      leftDrawer: true
     }
   }
 }
